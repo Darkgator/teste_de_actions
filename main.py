ec177f345caabd741f59b326355a67c85ea79afe
Darkgator <abm.mecatronica@gmail.com>
2025-11-26 20:15:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class AssinaturaRequest(BaseModel):
-    texto: str
+class BpmnRequest(BaseModel):
+    conteudo: str
 
-class AssinaturaResponse(BaseModel):
-    resultado: str
+class BpmnResponse(BaseModel):
+    conteudo: str
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-@app.post("/assinatura", response_model=AssinaturaResponse)
-def criar_assinatura(body: AssinaturaRequest):
-    texto_original = body.texto
-    texto_assinado = f"{texto_original} ASSINATURA FODA REALIZADA 123456"
-    return {"resultado": texto_assinado}
+@app.post("/bpmn", response_model=BpmnResponse)
+def processar_bpmn(body: BpmnRequest):
+    return {"conteudo": body.conteudo}
